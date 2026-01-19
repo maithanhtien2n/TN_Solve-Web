857f184a4b8a2914af45e906c0203e924378adce
Tien Thanh <tienmt@imt-soft.com>
2026-01-19 11:18:23 +0700

    update code

--- pages/video/[id].vue
+++ pages/video/[id].vue
@@ -50,11 +50,9 @@ const productId = computed(() =>
 
 const isError = computed(() =>
   Boolean(
-    (productId.value &&
+    productId.value &&
       formData.messages.length &&
-      formData.messages[formData.messages.length - 1]?.color === "error") ||
-      (formData.messages[formData.messages.length - 1]?.color === "success" &&
-        !formData.video)
+      formData.messages[formData.messages.length - 1]?.color === "error"
   )
 );
 
