8220d18f59208e7cda5512c0945e7338aafc2b42
Tien Thanh <tienmt@imt-soft.com>
2026-01-19 12:25:27 +0700

    update code

--- pages/video/[id].vue
+++ pages/video/[id].vue
@@ -32,7 +32,7 @@ const formData = reactive<any>({
   value: "",
   frameRate: "horizontal",
   modelVideo: "veo3_fast",
-  videoMode: "character_preservation",
+  videoMode: "movie",
   videoStyle: "general",
   videoDuration: "8",
   author: "",
