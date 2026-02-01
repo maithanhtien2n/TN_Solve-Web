10851431b7350dcb69a3b35a5b0affb68f4d7bc7
Tien Thanh <tienmt@imt-soft.com>
2026-02-01 14:46:53 +0700

    update code

--- components/layouts/AppFooter.vue
+++ components/layouts/AppFooter.vue
@@ -75,15 +75,8 @@ onMounted(async () => {
                     v-for="(item, index) in [
                       { title: 'Tài liệu', value: 'documents' },
                       { title: 'Hướng dẫn', value: 'tutorial' },
-                      { title: 'Đăng ký dịch vụ', value: 'payment' },
-                      ...(version
-                        ? []
-                        : [
-                            {
-                              title: 'Tạo trò chơi dạy học',
-                              value: 'tnsolve-teacher',
-                            },
-                          ]),
+                      { title: 'Công cụ đa năng', value: 'tnsolve-teacher' },
+                      { title: 'Đăng ký/gia hạn dịch vụ', value: 'payment' },
                     ]"
                     :key="index"
                     class="cursor-pointer"
