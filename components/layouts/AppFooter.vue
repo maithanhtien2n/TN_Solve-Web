635113f7914de7827d437c04f75adb59be579c4c
Tien Thanh <tienmt@imt-soft.com>
2026-02-04 09:23:04 +0700

    update code

--- components/layouts/AppFooter.vue
+++ components/layouts/AppFooter.vue
@@ -75,7 +75,15 @@ onMounted(async () => {
                     v-for="(item, index) in [
                       { title: 'Tài liệu', value: 'documents' },
                       { title: 'Hướng dẫn', value: 'tutorial' },
-                      { title: 'Công cụ đa năng', value: 'tnsolve-teacher' },
+                      ...(version
+                        ? []
+                        : [
+                            {
+                              title: 'Công cụ đa năng',
+                              value: 'tnsolve-teacher',
+                            },
+                          ]),
+
                       { title: 'Đăng ký/gia hạn dịch vụ', value: 'payment' },
                     ]"
                     :key="index"
