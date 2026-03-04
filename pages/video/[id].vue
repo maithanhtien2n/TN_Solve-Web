5b2339c6f23e033400eacff84727e61429fade58
Tien Thanh <tienmt@imt-soft.com>
2026-03-04 10:32:54 +0700

    update code

--- pages/video/[id].vue
+++ pages/video/[id].vue
@@ -224,7 +224,31 @@ const videoDurationOptions = computed(() => {
       "custom_process",
     ].includes(formData.videoMode)
   ) {
-    const shortVideoValues = ["1", "2", "3", "4", "5", "6", "7", "8"];
+    const shortVideoValues = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+    ];
     return allOptions.filter((option: any) =>
       shortVideoValues.includes(option.value)
     );
