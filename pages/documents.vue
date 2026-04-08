01efa5875bce6e0bcbdf9a0586cf75a774739d62
Tien Thanh <tienmt@imt-soft.com>
2026-04-09 00:27:08 +0700

    update code

--- pages/documents.vue
+++ pages/documents.vue
@@ -12,62 +12,63 @@ const documents = [
       "https://gemini.google.com/gem/1gc0fwxS3YpkFCa9c7ku2bL-XZ5PBVXm-?usp=sharing",
   },
   {
-    title: "🎬 Đạo Diễn Làm Phim Đa Vũ Trụ",
+    title: "🎬 AI Làm Phim Đa Vũ Trụ (Làm phim)",
     value:
       "https://gemini.google.com/gem/1xcYJ7af0H8S1NxE0n1IKRsFZX2y_qoIl?usp=sharing",
   },
   {
-    title: "📖 Đạo Diễn Phim Hoạt Hình AI Quà Tặng Cuộc Sống",
-    value:
-      "https://gemini.google.com/gem/18JVkNtXesH43TRA1zc1pC2_SEWAe4sdb?usp=sharing",
-  },
-  {
-    title: "🎨 Đạo Diễn Hoạt Hình Giáo Dục Trẻ Em Nói Tiếng Anh",
-    value:
-      "https://gemini.google.com/gem/1ovviMlY7omu3CBx1jgj97oy1q-EpT0UG?usp=sharing",
-  },
-  {
-    title: "✨ Đạo Diễn Hiệu Ứng Pháo Hoa POV",
-    value:
-      "https://gemini.google.com/gem/1VT0_eNcuQBNLw3OOb0yxnuLTvHG9rv-s?usp=sharing",
-  },
-  {
-    title: '🎭 Đạo Diễn 3D "Trái Cây Nổi Giận"',
-    value:
-      "https://gemini.google.com/gem/19V5yizpIG9fp5hfjROVBxkV-EkYab5mv?usp=sharing",
-  },
-  {
-    title: "📜 AI Kịch Bản Nhân Vật Kể Chuyện",
-    value:
-      "https://gemini.google.com/gem/1QzEAOsmWa0WKefSGy2QImSfEkW7YnEOc?usp=sharing",
-  },
-  {
-    title: "🍌 Bro Khỉ Tâm Sự Chuyện Đàn Ông",
-    value:
-      "https://gemini.google.com/gem/1cuZ3qYU3GkfR25YDXc1fPp1wk3rqGfw9?usp=sharing",
-  },
-  {
-    title: "🎵 Đạo Diễn AI Tạo MV Ca Nhạc Chuyên Nghiệp",
-    value:
-      "https://gemini.google.com/gem/1k-GUzKKA-pB_Xrc0sqAWRqLdtVKMvbFJ?usp=sharing",
-  },
-  {
-    title: "⏳ Fast-Forward Master (Bậc thầy tua nhanh)",
-    value:
-      "https://gemini.google.com/gem/1VhlbwN5fQTUUOZ1IYQERbqS9NZ1LmNSu?usp=sharing",
-  },
-  {
-    title: "📦 Tạo Video Quảng Cáo Xoay Sản Phẩm",
-    value:
-      "https://gemini.google.com/gem/1VVX-4ZWiP-JoJJP5CVTqcZxdBi2_8DMc?usp=sharing",
-  },
-  {
-    title: "📝 AI Chuyên Làm Video Podcast Triết Lý",
+    title:
+      "📝 AI Chuyên Làm Video Podcast Triết Lý (Đồng bộ nhân vật, Tùy chỉnh nhân vật)",
     value:
       "https://gemini.google.com/gem/1FZkD9kDuDCE-AvnPIt4KNrEa1Ih_XjIK?usp=sharing",
   },
   {
-    title: "📝 Xưởng Phim Cinematic AI (Pro Director)",
+    title: "📖 AI Phim Hoạt Hình AI Quà Tặng Cuộc Sống (Làm phim)",
+    value:
+      "https://gemini.google.com/gem/18JVkNtXesH43TRA1zc1pC2_SEWAe4sdb?usp=sharing",
+  },
+  {
+    title: "🍌 Bro Khỉ Tâm Sự Chuyện Đàn Ông (Làm phim)",
+    value:
+      "https://gemini.google.com/gem/1cuZ3qYU3GkfR25YDXc1fPp1wk3rqGfw9?usp=sharing",
+  },
+  {
+    title: "🎨 AI Hoạt Hình Giáo Dục Trẻ Em Nói Tiếng Anh (Làm phim)",
+    value:
+      "https://gemini.google.com/gem/1ovviMlY7omu3CBx1jgj97oy1q-EpT0UG?usp=sharing",
+  },
+  {
+    title: "✨ AI Hiệu Ứng Pháo Hoa POV (Làm phim)",
+    value:
+      "https://gemini.google.com/gem/1VT0_eNcuQBNLw3OOb0yxnuLTvHG9rv-s?usp=sharing",
+  },
+  {
+    title: "🎭 AI 3D Trái Cây Dạy Đạo Lý (Làm phim)",
+    value:
+      "https://gemini.google.com/gem/19V5yizpIG9fp5hfjROVBxkV-EkYab5mv?usp=sharing",
+  },
+  {
+    title: "📜 AI Kịch Bản Nhân Vật Kể Chuyện (Đồng bộ cảnh quay)",
+    value:
+      "https://gemini.google.com/gem/1QzEAOsmWa0WKefSGy2QImSfEkW7YnEOc?usp=sharing",
+  },
+  {
+    title: "🎵 AI Tạo MV Ca Nhạc Chuyên Nghiệp (Tùy chỉnh cảnh quay)",
+    value:
+      "https://gemini.google.com/gem/1k-GUzKKA-pB_Xrc0sqAWRqLdtVKMvbFJ?usp=sharing",
+  },
+  {
+    title: "⏳ AI Bậc thầy tua nhanh (Tùy chỉnh quy trình)",
+    value:
+      "https://gemini.google.com/gem/1VhlbwN5fQTUUOZ1IYQERbqS9NZ1LmNSu?usp=sharing",
+  },
+  {
+    title: "📦 Tạo Video Quảng Cáo Xoay Sản Phẩm (Tùy chỉnh nhân vật)",
+    value:
+      "https://gemini.google.com/gem/1VVX-4ZWiP-JoJJP5CVTqcZxdBi2_8DMc?usp=sharing",
+  },
+  {
+    title: "📝 Xưởng Phim Cinematic AI (Đồng bộ nhân vật, Tùy chỉnh nhân vật)",
     value:
       "https://gemini.google.com/gem/1a6CxQz_FAGG4Ks_ZOy87PVBoBD3lv97C?usp=sharing",
   },
